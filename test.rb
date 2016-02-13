34988542f7333d4b62d84e1e69a1dd442081d9d9
tkhsh <uki.thashi@gmail.com>
2016-02-14 01:09:22 +0900

    test: Add tests about parsing integer

--- test.rb
+++ test.rb
@@ -7,6 +7,19 @@ class TestParser < Minitest::Test
   include RScheme::LispObject
 
   def test_read_expr
-    # todo
+    # integer
+    StringIO.open("1 -1 10") do |strio|
+      parser = Parser.new(strio)
+
+      expr1 = parser.read_expr
+      assert_equal 1, expr1.value
+      assert_equal Type::INT, expr1.type
+      expr2 = parser.read_expr
+      assert_equal -1, expr2.value
+      assert_equal Type::INT, expr2.type
+      expr3 = parser.read_expr
+      assert_equal 10, expr3.value
+      assert_equal Type::INT, expr3.type
+    end
   end
 end
