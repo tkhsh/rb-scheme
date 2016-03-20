2eb703891ab49372a6b9286addbb7efd6c010f3e
tkhsh <uki.thashi@gmail.com>
2016-03-21 03:24:37 +0900

    Primitive: add subr_pair?

--- rb-scheme.rb
+++ rb-scheme.rb
@@ -549,6 +549,15 @@ module RbScheme
       end
     end
 
+    def subr_pair?
+      lambda do |args, env|
+        raise "Malformed pair?" unless args.count == 1
+        fst = args.car
+        return LFalse unless fst.type == Type::CELL
+        LTrue
+      end
+    end
+
     def arithmetic_proc(op)
       lambda do |args, env|
         args.each do |e|
@@ -630,6 +639,7 @@ module RbScheme
       add_subrutine!(env, "list", subr_list)
       add_subrutine!(env, "eq?", subr_eq?)
       add_subrutine!(env, "list?", subr_list?)
+      add_subrutine!(env, "pair?", subr_pair?)
       add_subrutine!(env, "+", subr_plus)
       add_subrutine!(env, "-", subr_minus)
       add_subrutine!(env, "*", subr_mul)
