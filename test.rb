34fc6ac06fe663253f80fe71bbb5389bddc7556e
tkhsh <uki.thashi@gmail.com>
2016-03-27 14:42:16 +0900

    test: Use assert_instance_of to test lisp object type

--- test.rb
+++ test.rb
@@ -12,13 +12,13 @@ class TestParser < Minitest::Test
 
       expr1 = parser.read_expr
       assert_equal 1, expr1.value
-      assert_equal Type::INT, expr1.type
+      assert_instance_of LInt, expr1
       expr2 = parser.read_expr
       assert_equal -1, expr2.value
-      assert_equal Type::INT, expr2.type
+      assert_instance_of LInt, expr2
       expr3 = parser.read_expr
       assert_equal 10, expr3.value
-      assert_equal Type::INT, expr3.type
+      assert_instance_of LInt, expr3
     end
 
     # symbol
@@ -27,10 +27,10 @@ class TestParser < Minitest::Test
 
       expr1 = parser.read_expr
       assert_equal "a", expr1.name
-      assert_equal Type::SYMBOL, expr1.type
+      assert_instance_of LSymbol, expr1
       expr2 = parser.read_expr
       assert_equal "bc", expr2.name
-      assert_equal Type::SYMBOL, expr2.type
+      assert_instance_of LSymbol, expr2
     end
 
     # quote
@@ -53,12 +53,12 @@ class TestParser < Minitest::Test
       assert_equal 1, expr1.car.value
       assert_equal 2, expr1.cdr.car.value
       assert_equal 3, expr1.cdr.cdr.car.value
-      assert_equal Type::NIL, expr1.cdr.cdr.cdr.type
+      assert_instance_of LNil, expr1.cdr.cdr.cdr
       expr2 = parser.read_expr
       assert_equal "a", expr2.car.name
-      assert_equal Type::NIL, expr2.cdr.type
+      assert_instance_of LNil, expr2.cdr
       expr3 = parser.read_expr
-      assert_equal Type::NIL, expr3.type
+      assert_instance_of LNil, expr3
     end
 
     # dotted list
@@ -79,9 +79,9 @@ class TestParser < Minitest::Test
       parser = Parser.new(strio)
 
       expr1 = parser.read_expr
-      assert_equal Type::TRUE, expr1.type
+      assert_instance_of LTrue, expr1
       expr2 = parser.read_expr
-      assert_equal Type::FALSE, expr2.type
+      assert_instance_of LFalse, expr2
     end
   end
 end
@@ -107,7 +107,7 @@ class TestExecuter < Minitest::Test
       @executer.set_source!(strio)
 
       result = eval_next(@env)
-      assert_equal Type::INT, result.type
+      assert_instance_of LInt, result
       assert_equal 10, result.value
     end
 
@@ -116,10 +116,10 @@ class TestExecuter < Minitest::Test
       @executer.set_source!(strio)
 
       result1 = eval_next(@env)
-      assert_equal Type::INT, result1.type
+      assert_instance_of LInt, result1
       assert_equal 1, result1.value
       result2 = eval_next(@env)
-      assert_equal Type::INT, result2.type
+      assert_instance_of LInt, result2
       assert_equal 2, result2.value
     end
 
@@ -128,7 +128,7 @@ class TestExecuter < Minitest::Test
       @executer.set_source!(strio)
 
       result = eval_next(@env)
-      assert_equal Type::INT, result.type
+      assert_instance_of LInt, result
       assert_equal 70, result.value
     end
 
@@ -138,7 +138,7 @@ class TestExecuter < Minitest::Test
 
       eval_next(@env)
       result = eval_next(@env)
-      assert_equal Type::INT, result.type
+      assert_instance_of LInt, result
       assert_equal 3, result.value
     end
 
@@ -153,7 +153,7 @@ class TestExecuter < Minitest::Test
 
       eval_next(@env)
       result = eval_next(@env)
-      assert_equal Type::FALSE, result.type
+      assert_instance_of LFalse, result
     end
 
     # syntax_set!
@@ -178,7 +178,7 @@ class TestExecuter < Minitest::Test
       @executer.set_source!(strio)
 
       result = eval_next(@env)
-      assert_equal Type::NIL, result.type
+      assert_instance_of LNil, result
     end
   end
 end
