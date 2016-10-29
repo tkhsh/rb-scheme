0b3273e6a1c4235d8828708978d0a8216c1a0848
tkhsh <uki.thashi@gmail.com>
2016-10-29 15:44:50 +0900

    test: Add tests for vm_eval

--- test/test_evaluator.rb
+++ test/test_evaluator.rb
@@ -99,17 +99,43 @@ class TestExecuter < Minitest::Test
     vm_eval_exprs = <<-EXPRS
     (vm_eval ((lambda (x) x) 1))
     (vm_eval ((lambda (a b) ((lambda (x y) x) b 3)) 4 5))
+    (vm_eval '(1 2))
+    (vm_eval ((lambda (x) (set! x 10)) 1))
+    (vm_eval ((lambda (x) (if 1 x (set! x 2)))
+              3))
+    (vm_eval ((lambda (x) (call/cc (lambda (s) (s 9)))) 4))
     EXPRS
     StringIO.open(vm_eval_exprs) do |strio|
       @executer.set_source!(strio)
 
+      # (vm_eval ((lambda (x) x) 1))
       result1 = eval_next(@env)
       assert_instance_of LInt, result1
       assert_equal 1, result1.value
 
+      # (vm_eval ((lambda (a b) ((lambda (x y) x) b 3)) 4 5))
       result2 = eval_next(@env)
       assert_instance_of LInt, result2
       assert_equal 5, result2.value
+
+      # (vm_eval '(1 2))
+      result3 = eval_next(@env)
+      assert_instance_of LCell, result3
+      assert_equal 1, result3.car.value
+      assert_equal 2, result3.cadr.value
+
+      # (vm_eval ((lambda (x) (set! x 10) x) 1))
+      result4 = eval_next(@env)
+      assert_equal 10, result4.value
+
+      # (vm_eval ((lambda (x) (if 1 x (set! x 2)) x)
+      #           3))
+      result5 = eval_next(@env)
+      assert_equal 3, result5.value
+
+      # (vm_eval ((lambda (x) (call/cc (lambda (s) 3))) 4))
+      result6 = eval_next(@env)
+      assert_equal 9, result6.value
     end
 
   end
