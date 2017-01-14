3a46dd31d5873dc5ea3fed3c9b0d8c644fca1fd4
tkhsh <uki.thashi@gmail.com>
2017-01-14 13:02:07 +0900

    evaluator: Encapsulate methods

--- lib/rb-scheme/evaluator.rb
+++ lib/rb-scheme/evaluator.rb
@@ -2,39 +2,6 @@ module RbScheme
   class Evaluator
     include Helpers
 
-    def lookup_variable(var, env)
-      env.each do |frame|
-        frame.each do |bind|
-          return bind if var.name == bind.car.name
-        end
-      end
-
-      raise "Unbound variable - #{var.name}"
-    end
-
-    def map_eval(lst, env)
-      return LNil.instance unless LCell === lst && lst.list?
-      result_array = lst.map { |e| eval(e, env) }
-      convert_to_list(result_array)
-    end
-
-    def progn(expr_list, env)
-      expr_list.map { |expr| eval(expr, env) }.last
-    end
-
-    def extend_env(env, vars, vals)
-      frame = LNil.instance
-      vars.to_a.zip(vals.to_a) do |var, val|
-        frame = acons(var, val, frame)
-      end
-      cons(frame, env)
-    end
-
-    def apply(fn, args, env)
-      extended = extend_env(env, fn.params, args)
-      progn(fn.body, extended)
-    end
-
     def eval(obj, env)
       case obj
       when LInt, LTrue, LFalse, LNil
@@ -64,6 +31,41 @@ module RbScheme
         raise "Unexpected type - #{obj}"
       end
     end
+
+    def lookup_variable(var, env)
+      env.each do |frame|
+        frame.each do |bind|
+          return bind if var.name == bind.car.name
+        end
+      end
+
+      raise "Unbound variable - #{var.name}"
+    end
+
+    private
+
+      def map_eval(lst, env)
+        return LNil.instance unless LCell === lst && lst.list?
+        result_array = lst.map { |e| eval(e, env) }
+        convert_to_list(result_array)
+      end
+
+      def progn(expr_list, env)
+        expr_list.map { |expr| eval(expr, env) }.last
+      end
+
+      def extend_env(env, vars, vals)
+        frame = LNil.instance
+        vars.to_a.zip(vals.to_a) do |var, val|
+          frame = acons(var, val, frame)
+        end
+        cons(frame, env)
+      end
+
+      def apply(fn, args, env)
+        extended = extend_env(env, fn.params, args)
+        progn(fn.body, extended)
+      end
 
   end # Evaluator
 end # RbScheme
