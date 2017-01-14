78ff7bca4a22079bd4bcfe9c573e4727f194f4e1
tkhsh <uki.thashi@gmail.com>
2017-01-14 19:29:44 +0900

    evaluator: Add vm_eval

--- lib/rb-scheme/evaluator.rb
+++ lib/rb-scheme/evaluator.rb
@@ -1,6 +1,25 @@
 module RbScheme
   class Evaluator
+    extend Forwardable
     include Helpers
+    include Symbol
+
+    def_delegator :@compiler, :compile
+    def_delegator :@vm, :exec, :vm_exec
+
+    def initialize
+      @compiler = Compiler.new
+      @vm = VM.new
+    end
+
+    def vm_eval(obj)
+      c = compile(obj, list, Set.new, list(intern("halt")))
+      vm_exec(list,
+              c,
+              0,
+              list,
+              0)
+    end
 
     def eval(obj, env)
       case obj
