6c792356977c7404f7269f1f7cbd52f4f31e39ce
tkhsh <uki.thashi@gmail.com>
2017-01-15 15:09:46 +0900

    test: Add TestHelper

--- test/helper.rb
+++ test/helper.rb
@@ -2,3 +2,13 @@ require 'minitest/autorun'
 require 'stringio'
 require 'rb-scheme'
 
+module TestHelper
+  include RbScheme
+
+  def parse_string(str_expr)
+    StringIO.open(str_expr) do |io|
+      Parser.read_expr(io)
+    end
+  end
+
+end
